bb478387f88372743355bb612bdf3b0e71bc1010
Vladimir <urkytsk3@gmail.com>
2023-06-13 16:25:18 +0800

    Добавил модель для конфигурационного файла

---
+++ ConfigurationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotCookies
+{
+    public class ConfigurationModel
+    {
+        public int RepeatCount { get; set; }
+        public string? SearchQueries { get; set; }
+        public int MinSearchCount { get; set; }
+        public int MaxSearchCount { get; set; }
+        public int MinSiteVisitCount { get; set; }
+        public int MaxSiteVisitCount { get; set; }
+        public int MinTimeSpent { get; set; }
+        public int MaxTimeSpent { get; set; }
+        public string? ProfileGroupName { get; set; }
+    }
+
+}
